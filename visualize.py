3c2fbb93384f2a60e952f1dac7d9ea2d23cc2bf0
Jonas Schiefner <jonas.schiefner@me.com>
2020-08-21 13:41:58 +0200

    add visualize.py

---
+++ visualize.py
@@ -0,0 +1,36 @@
+# %% parse arguments
+
+from argparse import ArgumentParser
+from os import path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+parser = ArgumentParser()
+parser.add_argument('-m', '--model', required=True, help='Model Name')
+args = vars(parser.parse_args())
+modelDataPath = f'models/{args["model"]}.csv'
+
+if not path.exists(modelDataPath):
+    print(
+        f'[ERROR] the model "{args["model"]} has not been trained yet.',
+        f'Please train the model first and re-run this file.',
+    )
+    exit(1)
+
+# %% run
+
+modelData = pd.read_csv(modelDataPath, index_col=0)
+epochs = np.arange(0, len(modelData))
+plt.style.use('ggplot')
+plt.title(f'Training Loss/Accuracy for {args["model"]} model')
+plt.xlabel('# Epochs')
+plt.ylabel('Loss/Accuracy')
+
+plt.plot(epochs, modelData.loss, label='loss')
+plt.plot(epochs, modelData.accuracy, label='accuracy')
+plt.plot(epochs, modelData.val_loss, label='val_loss')
+plt.plot(epochs, modelData.val_accuracy, label='val_accuracy')
+
+plt.legend(loc='center right')
+plt.show()
