6c17bb245f39d953e8272b48bbfa996a2b8fdeb0
Jonas Schiefner <jonas.schiefner@me.com>
2020-08-23 15:06:14 +0200

    properly split dataset and add ModelCheckpoint callback
    split in test + (train, validation) data, test for validation after training, validation for validation during training

--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 # %% parse arguments
 
 from argparse import ArgumentParser
-from os import path, mkdir
+from os import path, mkdir, environ
+environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 parser = ArgumentParser()
-parser.add_argument("-d",'--dataset', default=".", help='path to input folder, defaults to current folder if no path given')
-parser.add_argument("-m","--model", default="VGG16", help="specify optional network")
+parser.add_argument('dataset', help='path to input folder')
+parser.add_argument("-m", "--model", default="VGG16", help="specify optional network")
 args = vars(parser.parse_args())
+# args = {'dataset': '.', 'model': 'VGG16'}
 
 # metadata check
 if not path.exists(args['dataset']):
@@ -61,8 +63,10 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Input, AveragePooling2D, Flatten, Dense, Dropout, GaussianNoise
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
 from cv2 import imread, cvtColor, resize, COLOR_BGR2RGB
 from glob import glob
 import pandas as pd
@@ -76,7 +80,7 @@ session = tf.compat.v1.Session(config=config)
 
 # initialize some training parameters
 INIT_LR = 1e-3
-EPOCHS = 25
+EPOCHS = 25 # TODO: adjust
 BS = 8
 
 if modelExists:
@@ -88,7 +92,9 @@ if modelExists:
     print(f'[INFO] trainEpochs: {trainEpochs}')
     modelPaths = glob(f'models/{args["model"]}_*.h5')
     print(f'[INFO] modelPaths: {modelPaths}')
-    model = load_model(modelPaths[-1]) # load latest model
+    latestModelPath = modelPaths[-1]
+    model = load_model(latestModelPath) # load latest model
+    print(f'[INFO] successfully loaded "{latestModelPath}"')
 else:
     print(f'[INFO] Model does not exist yet, creating a new one')
     baseModel = modelFunc(weights='imagenet', include_top=False, input_tensor=Input(shape=(224, 224, 3)))
@@ -110,13 +116,12 @@ else:
 
 if trainEpochs <= 0:
     print(f'[INFO] network is already trained on {EPOCHS} epochs, exiting')
-    exit(0)
 
 # %% prepare data
 print('[INFO] prepare data')
 
 metadata = pd.read_csv(path.join(args['dataset'], "metadata.csv"), usecols=['File', 'Covid'], dtype={'File': np.str, 'Covid': np.bool})
-metadata = metadata[1000:1100] # for now only use 100 samples (10 positive, 90 negative)
+# metadata = metadata[1000:1100] # for now only use 100 samples (10 positive, 90 negative) # TODO: comment out before comitting
 
 covid = metadata[metadata['Covid'] == True]
 healthy = metadata[metadata['Covid'] == False]
@@ -135,6 +140,8 @@ for idx, (file, covid) in metadata.iterrows():
 
 data = np.array(data) / 255.0
 labels = np.array(labels)
+dataLength = len(data)
+print(f'[INFO] successfully loaded {dataLength} images')
 
 print('[INFO] encode labels')
 
@@ -144,19 +151,32 @@ labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
 
 # datasplit
+print('[INFO] splitting data')
+# use last 1/3rd of data for validation
+
+third = (dataLength // 3) * 2
+trainValidationData = data[:third]
+trainValidationLabels = labels[:third]
+testData = data[third:]
+testLabels = labels[third:]
+
+print(f'[INFO] selected {len(testData)} images for validation after training')
+
 # random_stateint Controls the shuffling applied to the data before applying the split.
 # pass an int for reproducible output across multiple function calls. See Glossary.
-(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.2, stratify=labels ) # random_state=42
+(trainX, valX, trainY, valY) = train_test_split(trainValidationData, trainValidationLabels, test_size=0.2, stratify=trainValidationLabels) # random_state=42
+print(f'[INFO] selected {len(trainX)} images for training and {len(valX)} images for validation (during training)')
 
 # %% train model
 
 # initialize the training data augmentation object
-trainAug = ImageDataGenerator(rotation_range=10,      # adapt rotation range? wie schief ist so ein röntgenbild wohl maxial aufgenommen worden
-                              horizontal_flip=True,   # cc: die schieferen scheinen so maximal 20, die allermeisten aber <5; 10 ist denke ich guter Mittelweg
-                              fill_mode='nearest',    # Testweise bessere performance als constant fill
-                              width_shift_range=0.1,  # Horizontal sind die Bilder größtenteils gut zentriert
-                              height_shift_range=0.2) # Vertikal tendenziell etwas schlechter
-                              #zoom_range=0.2)         # 1.0 +- zoom_range, kann also raus oder reinzoomen
+# trainAug = ImageDataGenerator(rotation_range=10,      # adapt rotation range? wie schief ist so ein röntgenbild wohl maxial aufgenommen worden
+#                               horizontal_flip=True,   # cc: die schieferen scheinen so maximal 20, die allermeisten aber <5; 10 ist denke ich guter Mittelweg
+#                               fill_mode='nearest',    # Testweise bessere performance als constant fill
+#                               width_shift_range=0.1,  # Horizontal sind die Bilder größtenteils gut zentriert
+#                               height_shift_range=0.2) # Vertikal tendenziell etwas schlechter
+#                               #zoom_range=0.2)         # 1.0 +- zoom_range, kann also raus oder reinzoomen
+trainAug = ImageDataGenerator(rotation_range=15, fill_mode='nearest')
 opt = Adam(lr=INIT_LR, decay=INIT_LR / trainEpochs)
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
@@ -166,27 +186,56 @@ try:
     model.fit(
         trainAug.flow(trainX, trainY, batch_size=BS),
         steps_per_epoch=len(trainX) // BS,
-        validation_data=(testX, testY),
-        validation_steps=len(testX) // BS,
+        validation_data=(valX, valY),
+        validation_steps=len(valX) // BS,
         epochs=trainEpochs,
+        callbacks=[ModelCheckpoint(
+            filepath=f'models/checkpoints/{args["model"]}_' + 'epoch{epoch}_' +'ckpt-loss={loss:.2f}.h5',
+            monitor='val_loss',
+            save_weights_only=True,
+            save_best_only=True,
+            period=3,
+        )]
     )
 except KeyboardInterrupt:
     print(f'\n[INFO] interrupted Training at epoch: {len(model.history.epoch)}')
 
+# save history and epochs for later
+# as history gets deleted when model.predict() is called
+history = model.history.history.copy()
+epochs = len(model.history.epoch)
+
+# %% validate model
+
+print('[INFO] Evaluating trained network')
+predictions = np.argmax(model.predict(testData, batch_size=BS), axis=1)
+
+print(classification_report(testLabels.argmax(axis=1), predictions, target_names=lb.classes_))
+cm = confusion_matrix(testLabels.argmax(axis=1), predictions)
+total = sum(sum(cm))
+acc = (cm[0, 0] + cm[1, 1]) / total
+sensitivity = cm[0, 0] / (cm[1, 0] + cm[1, 1])
+specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
+
+print(f'TP: {cm[0, 0]}, FN: {cm[0, 1]}')
+print(f'FP: {cm[1, 0]}, TN: {cm[1, 1]}')
+print('acc: {:.4f}'.format(acc))
+print('sensitivity: {:.4f}'.format(sensitivity))
+print('specificity: {:.4f}'.format(specificity))
+
 # %% serialize model and csv
 
 if modelExists:
-    epoch = len(model.history.epoch) + trainedEpochs
-    print(f'[INFO] epoch: {epoch}')
-    modelData = modelData.append(pd.DataFrame(model.history.history))
+    epochs += trainedEpochs
+    print(f'[INFO] epoch: {epochs}')
+    modelData = modelData.append(pd.DataFrame(history))
 else:
-    epoch = len(model.history.epoch)
-    print(f'[INFO] epoch: {epoch}')
-    modelData = pd.DataFrame(model.history.history)
+    print(f'[INFO] epoch: {epochs}')
+    modelData = pd.DataFrame(history)
 
 if not path.exists('models'):
     mkdir('models') # create models directory if it doesnt exist yet
-modelPath = f'models/{args["model"]}_{epoch}.h5'
+modelPath = f'models/{args["model"]}_{epochs}.h5'
 csvPath = f'models/{args["model"]}.csv'
 print(f'[INFO] saving model to: {modelPath}, saving csv to: {csvPath}')
 model.save(modelPath, save_format='h5')
