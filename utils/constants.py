fdb1f0bff0cdc1b8ef82947de9bcbf590c76f659
t_rade <t_rade03@wwu.de>
2020-09-20 16:36:23 +0200

    Revert "move baseModels out of train.py into utils.base_models.py"
    This reverts commit 9ffde918

--- utils/constants.py
+++ utils/constants.py
@@ -1,11 +1,8 @@
 import logging as log
 
-
 CLASSES = ['covid', 'healthy', 'other']
 IMG_DIMENSIONS = (224, 224)
 IMG_DIMENSIONS_3D = (224, 224, 3)
-
-
 
 def printSeparator(with_break=False):
     if with_break:
