073e9d5ccf8cf21b471389dbb3195cb8ac4cd372
Jonas Schiefner <jonas.schiefner@me.com>
2020-09-27 16:49:17 +0200

    minor improvements to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,11 +3,12 @@ from argparse import ArgumentParser
 from os import listdir, mkdir
 from os.path import exists, join
 import pandas as pd
+from utils.management import check_and_create_folder
 
 parser = ArgumentParser()
 parser.add_argument('dataset', help='path to input folder')
-parser.add_argument('-o', '--outputfolder', default='augmented images', help='path to output folder')
-parser.add_argument('-c', '--count', default=10, help='number of random augmentations for each image')
+parser.add_argument('-o', '--outputfolder', default='augmented_images', help='path to output folder')
+parser.add_argument('-c', '--count', default=10, type=int, help='number of random augmentations for each image')
 args = vars(parser.parse_args())
 
 # Subject to change
@@ -18,18 +19,20 @@ datagen = ImageDataGenerator(rotation_range=10,
                              height_shift_range=0.2, # Vertikal tendenziell etwas schlechter
                              zoom_range=0.2)
 
-if not exists(args['outputfolder']):
-    mkdir(args['outputfolder'])
+check_and_create_folder(args['outputfolder'])
+imagesOutPath = join(args['outputfolder'], 'images')
+check_and_create_folder(imagesOutPath)
 
 print('Augmenting...')
-for file in listdir(join(args['dataset'], 'images')):
+fileList = listdir(join(args['dataset'], 'images'))
+for file in fileList:
     if file[-4:] in (".jpg", "jpeg", ".png"):
         img = load_img(join(args['dataset'], 'images', file))  # PIL image
         x = img_to_array(img)  # numpy array with shape (3, 150, 150)
         x = x.reshape((1,) + x.shape)  # numpy array with shape (1, 3, 150, 150)
 
         i = 0
-        for batch in datagen.flow(x, batch_size=1, save_to_dir=args['outputfolder'],
+        for batch in datagen.flow(x, batch_size=1, save_to_dir=imagesOutPath,
                                   save_prefix=file, save_format='jpeg'):
             i += 1
             if i >= args['count']:
@@ -38,11 +41,12 @@ for file in listdir(join(args['dataset'], 'images')):
 # Second pass: we don't know the augemented file names beforehand,
 # so we iterate over the output folder once more to fill in metadata
 print('Writing metadata...')
-metadata = pd.read_csv(join(args['dataset'], "metadata.csv"))
-for file in listdir(args['outputfolder']):
+oldMetadata = pd.read_csv(join(args['dataset'], "metadata.csv"))
+metadata = pd.DataFrame(columns=oldMetadata.columns)
+for file in listdir(imagesOutPath):
     original = file.split(sep='_')[0]  # obtain the original file name for metadata lookup
-    data = metadata.loc[metadata["File"] == original]
+    data = oldMetadata.loc[oldMetadata["File"] == original]
     data.head()['File'] = file
     metadata = metadata.append(data)
 
-metadata.to_csv(join(args['outputfolder'], "metadata_aug.csv"))
+metadata.to_csv(join(args['outputfolder'], "metadata.csv"))
