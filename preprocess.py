da75128791756ef5699eb7df9e91d902a82a2058
Jonas Schiefner <jonas.schiefner@me.com>
2020-09-29 15:45:32 +0200

    add progress bar and re-add missing first pass in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,7 @@ from os.path import exists, join, splitext
 import pandas as pd
 from utils.management import check_and_create_folder
 import numpy as np
+from progress.bar import Bar
 
 parser = ArgumentParser()
 parser.add_argument('dataset', help='path to input folder')
@@ -26,7 +27,23 @@ check_and_create_folder(imagesOutPath)
 
 print('Augmenting...')
 metadata = pd.read_csv(join(args['dataset'], 'metadata.csv'))
+
 outdata = pd.DataFrame(columns=metadata.columns)
+
+with Bar('Processing images', max=len(metadata)) as bar:
+    for _, row in metadata.iterrows():
+        bar.next()
+        img = load_img(join(args['dataset'], 'images', row.File))  # PIL image
+        img = img_to_array(img)  # numpy array with shape (3, 150, 150)
+        img = img.reshape((1,) + img.shape)  # numpy array with shape (1, 3, 150, 150)
+
+        count = 0
+        maxCount = args['count'] * 20 if row.Covid else args['count']
+        for batch in datagen.flow(img, batch_size=1, save_to_dir=imagesOutPath,
+                                  save_prefix=row.File, save_format='jpeg'):
+            count += 1
+            if count >= maxCount:
+                break  # otherwise the generator would loop indefinitely
 
 print('Writing metadata...')
 # Second pass: we don't know the augemented file names beforehand,
