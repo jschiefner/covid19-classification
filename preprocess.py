be966407f5cfcaa6686ebc1229f9f0bf3ff0f965
Jonas Schiefner <jonas.schiefner@me.com>
2020-09-29 14:33:20 +0200

    multiply covid images more often than other in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,10 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 from argparse import ArgumentParser
 from os import listdir, mkdir
-from os.path import exists, join
+from os.path import exists, join, splitext
 import pandas as pd
 from utils.management import check_and_create_folder
+import numpy as np
 
 parser = ArgumentParser()
 parser.add_argument('dataset', help='path to input folder')
@@ -24,29 +25,17 @@ imagesOutPath = join(args['outputfolder'], 'images')
 check_and_create_folder(imagesOutPath)
 
 print('Augmenting...')
-fileList = listdir(join(args['dataset'], 'images'))
-for file in fileList:
-    if file[-4:] in (".jpg", "jpeg", ".png"):
-        img = load_img(join(args['dataset'], 'images', file))  # PIL image
-        x = img_to_array(img)  # numpy array with shape (3, 150, 150)
-        x = x.reshape((1,) + x.shape)  # numpy array with shape (1, 3, 150, 150)
+metadata = pd.read_csv(join(args['dataset'], 'metadata.csv'))
+outdata = pd.DataFrame(columns=metadata.columns)
 
-        i = 0
-        for batch in datagen.flow(x, batch_size=1, save_to_dir=imagesOutPath,
-                                  save_prefix=file, save_format='jpeg'):
-            i += 1
-            if i >= args['count']:
-                break  # otherwise the generator would loop indefinitely
-
+print('Writing metadata...')
 # Second pass: we don't know the augemented file names beforehand,
 # so we iterate over the output folder once more to fill in metadata
-print('Writing metadata...')
-oldMetadata = pd.read_csv(join(args['dataset'], "metadata.csv"))
-metadata = pd.DataFrame(columns=oldMetadata.columns)
 for file in listdir(imagesOutPath):
-    original = file.split(sep='_')[0]  # obtain the original file name for metadata lookup
-    data = oldMetadata.loc[oldMetadata["File"] == original]
+    original = file.split('_')[0]  # obtain the original file name for metadata lookup
+    data = metadata.loc[metadata['File'] == original]
     data.head()['File'] = file
-    metadata = metadata.append(data)
+    outdata = outdata.append(data)
 
-metadata.to_csv(join(args['outputfolder'], "metadata.csv"))
+outdata.index = np.arange(0, len(outdata))
+outdata.to_csv(join(args['outputfolder'], "metadata.csv"))
