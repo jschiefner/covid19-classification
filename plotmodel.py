7f5574dcc50a57069ffa6ca8a5e953b94e002821
t_rade <t_rade03@wwu.de>
2020-10-03 13:16:01 +0200

    bugs

---
+++ plotmodel.py
@@ -0,0 +1,19 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten, GlobalAveragePooling2D
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils.vis_utils import plot_model
+
+
+model = load_model("models/VGG16/epoch_2.h5")
+# baseModel = Sequential()
+# x = GlobalAveragePooling2D(name='global_avg_pool2d')
+# # x = Flatten(name='flatten')(baseModel.output)
+# x = Dense(256, activation='relu', name='fc1')(x)
+# x = Dropout(0.3)(x)
+# x = Dense(128, activation='relu', name='fc2')(x)
+# x = Dropout(0.5)(x)
+# x = Dense(3, activation='softmax', name='predictions')(x)
+
+plot_model(model, to_file='horizontalvgg16model.png',show_shapes=True,rankdir='TB')
